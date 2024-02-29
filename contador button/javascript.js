ded858e460f7a489c25bdcfd6de357036f1bc24a
Otávio Dantas Weiand <83969703+wotavio@users.noreply.github.com>
2024-02-29 13:55:13 -0300

    Add files via upload

--- contador button/javascript.js
+++ contador button/javascript.js
@@ -1,57 +1,31 @@
-// const value = document.querySelector('value');
-// const plusButton = document.querySelector('plus');
-// const minusButton = document.querySelector('minus');
-// const resetButton = document.querySelector('reset');
-
-// const updateValue = () => {
-//     value.innerHTML = count
-// }
-
-
-// let count = 0;
-// let intervalId = 0;
-
-// plusButton.addEventListener('mousedown', () => {
-// intervalId = setInterval(() => {
-//     count += 1;
-//     updateValue();
-// }, 100);
-// });
-
-
-// minusButton.addEventListener('mousedown', () => {
-//     intervalId = setInterval(() => {
-//         count -= 1;
-//         updateValue();
-//     }, 100);
-//     });
-
-
-// resetButton.addEventListener('click', () => {
-//         count = 0;
-//         updateValue();
-//     });
-
-// document.addEventListener('mouseup', () => clearInterval(intervalId))
 const counters = document.querySelectorAll('.counter');
+const totalValue = document.querySelector('.total-value');
 
-const updateValue = (counter, count) => {
-    const value = counter.querySelector('.value');
-    value.innerHTML = count;
-};
+function updateTotal() {
+    let total = 0;
+    counters.forEach(counter => {
+        total += parseInt(counter.querySelector('.value').textContent);
+    });
+    totalValue.textContent = total;
+}
 
-const startInterval = (counter, operation) => {
+function updateCounter(counter, operation) {
+    let count = parseInt(counter.dataset.count);
+    counter.dataset.count = operation(count);
+    counter.querySelector('.value').textContent = counter.dataset.count;
+    updateTotal();
+}
+
+function startInterval(counter, operation) {
     const intervalId = setInterval(() => {
-        let count = parseInt(counter.dataset.count);
-        counter.dataset.count = operation(count);
-        updateValue(counter, counter.dataset.count);
+        updateCounter(counter, operation);
     }, 100);
     counter.dataset.intervalId = intervalId;
-};
+}
 
-const stopInterval = (counter) => {
+function stopInterval(counter) {
     clearInterval(parseInt(counter.dataset.intervalId));
-};
+}
 
 document.addEventListener('mouseup', () => {
     counters.forEach(counter => stopInterval(counter));
@@ -61,13 +35,13 @@ counters.forEach(counter => {
     const plusButton = counter.querySelector('.plus');
     const minusButton = counter.querySelector('.minus');
     const resetButton = counter.querySelector('.reset');
-    
+
     plusButton.addEventListener('mousedown', () => startInterval(counter, count => count + 1));
-    
     minusButton.addEventListener('mousedown', () => startInterval(counter, count => count - 1));
     
     resetButton.addEventListener('click', () => {
         counter.dataset.count = 0;
-        updateValue(counter, counter.dataset.count);
+        counter.querySelector('.value').textContent = counter.dataset.count;
+        updateTotal();
     });
 });
